deaa09f8935761ce566f3edb4a7a47cf0b0c2b19
Ger Carney <Ger@noclipdigital.com>
2018-12-20 21:52:58 +0000

    Adding ui button

---
+++ components/UiButton.vue
@@ -0,0 +1,21 @@
+<template>
+<button class="ui-btn">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.ui-btn {
+    background-color: #f9bf62;
+    border: 5px solid #333;
+    font-family: 'Amatic SC', cursive;
+    font-size: 30px;
+    font-weight: bold;
+    position: relative;
+    width: 200px;
+
+    &:active {
+        background-color: #faddb3;
+    }
+}
+</style>
